5be77b99e4642e42fd0c4d4e0d9100a3a0f447ba
Elliott Saslow <elliottsaslow@MacBook-Pro.local>
2025-03-28 13:20:45 -0600

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import { ThemeProvider } from '@/context/ThemeContext'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -49,6 +50,7 @@ export default function RootLayout({
               {children}
             </main>
             <Footer />
+            <Analytics />
           </div>
         </ThemeProvider>
       </body>
